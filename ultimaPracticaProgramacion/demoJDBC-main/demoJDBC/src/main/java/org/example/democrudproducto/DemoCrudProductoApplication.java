ec8cfa07bf4cbd90f11b8687cf88df065a9ad5b7
Gerardgomez22 <gerardgomazn@campus.monlau.com>
2025-05-13 13:35:45 +0200

    Add files via upload

---
+++ ultimaPracticaProgramacion/demoJDBC-main/demoJDBC/src/main/java/org/example/democrudproducto/DemoCrudProductoApplication.java
@@ -0,0 +1,13 @@
+package org.example.democrudproducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudProductoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCrudProductoApplication.class, args);
+    }
+
+}
